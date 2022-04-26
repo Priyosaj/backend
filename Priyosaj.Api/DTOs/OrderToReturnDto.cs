894c1d46dbc13a86e982d00b1cb59edadd2e99c0
Souhardya Saha <souuhardyasaha98@gmail.com>
2022-04-26 21:04:39 +0600

    DTOs added

---
+++ Priyosaj.Api/DTOs/OrderToReturnDto.cs
@@ -0,0 +1,16 @@
+﻿using Priyosaj.Contacts.Entities.Order;
+
+namespace Priyosaj.Api.DTOs;
+
+public class OrderToReturnDto
+{
+    public int Id { get; set; }
+    public string BuyerEmail { get; set; }
+    public DateTimeOffset OrderDate { get; set; }
+    public ShippingAddress ShipToAddress { get; set; }
+    public string DeliveryMethod { get; set; }
+    public decimal ShippingPrice { get; set; }
+    public IReadOnlyList<OrderItemDto> OrderItems { get; set; }
+    public decimal Subtotal { get; set; }
+    public string Status { get; set; }
+}

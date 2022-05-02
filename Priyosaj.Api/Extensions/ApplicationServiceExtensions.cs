c6de3103fcb643691b0f90f8284bc332aa5eac26
SouhardyaSaha <souhardyasaha98@gmail.com>
2022-05-02 20:59:42 +0600

    Caching Implemented

--- Priyosaj.Api/Extensions/ApplicationServiceExtensions.cs
+++ Priyosaj.Api/Extensions/ApplicationServiceExtensions.cs
@@ -13,6 +13,7 @@ public static class ApplicationServiceExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
     {
+        services.AddSingleton<IResponseCacheService, ResponseCacheService>();
         services.AddSingleton<IConnectionMultiplexer>(c =>
         {
             var configuration = ConfigurationOptions.Parse(config.GetConnectionString("Redis"), true);
---
+++ Priyosaj.Contacts/Interfaces/IResponseCacheService.cs
@@ -0,0 +1,7 @@
+﻿namespace Priyosaj.Contacts.Interfaces;
+
+public interface IResponseCacheService
+{
+    Task CacheResponseAsync(string cacheKey, object response, TimeSpan timeToLive);
+    Task<string?> GetCachedResponse(string cacheKey);
+}

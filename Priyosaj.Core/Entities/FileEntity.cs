c9b202b6137be271d438556213b36c7b6f7094bc
SouhardyaSaha <souhardyasaha98@gmail.com>
2022-08-05 13:56:20 +0600

    FileEntity WIP

---
+++ Priyosaj.Core/Entities/FileEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Priyosaj.Core.Entities;
+
+public class FileEntity : ABaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public FileType Type { get; set; }
+    public string Url { get; set; } = string.Empty;
+}
+
+public enum FileType
+{
+    Image,
+}

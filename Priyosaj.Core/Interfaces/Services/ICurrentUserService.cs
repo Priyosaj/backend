a18d3c2a3f8ffc9c2156bf59c40bfa2e2c8d6892
SouhardyaSaha <souhardyasaha98@gmail.com>
2022-08-05 19:41:48 +0600

    Auth Check Added To CurrentUserService

--- Priyosaj.Core/Interfaces/Services/ICurrentUserService.cs
+++ Priyosaj.Core/Interfaces/Services/ICurrentUserService.cs
@@ -2,8 +2,10 @@ namespace Priyosaj.Core.Interfaces.Services;
 
 public interface ICurrentUserService
 {
-    string? Role { get; set; }
-    string? Email { get; set; }
-    string? UserName { get; set; }
-    Guid? UserId { get; set; }
+    string Role { get; set; }
+    string Email { get; set; }
+    string UserName { get; set; }
+    Guid UserId { get; set; }
+    public void ValidateIfEditor();
+    public void ValidateIfCustomer();
 }
--- Priyosaj.Service/CurrentUserService.cs
+++ Priyosaj.Service/CurrentUserService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
+using Priyosaj.Core.Constants;
 using Priyosaj.Core.Interfaces.Services;
 
 namespace Priyosaj.Service;
@@ -9,17 +10,48 @@ public class CurrentUserService : ICurrentUserService
     public CurrentUserService(IHttpContextAccessor httpContextAccessor)
     {
         var claimsPrincipal = httpContextAccessor.HttpContext.User;
+
+        /*
+        var userId = claimsPrincipal.FindFirstValue("userId");
+        var userName = claimsPrincipal.FindFirstValue(ClaimTypes.GivenName);
+        var email = claimsPrincipal.FindFirstValue(ClaimTypes.Email);
+        var role = claimsPrincipal.FindFirstValue(ClaimTypes.Role);
+
+        if (userId == null || userName == null || email == null || role == null)
+        {
+            throw new UnauthorizedAccessException("Please log in");
+        }
+        */
+
         UserId = Guid.Parse(claimsPrincipal.FindFirstValue("userId"));
         UserName = claimsPrincipal.FindFirstValue(ClaimTypes.GivenName);
         Email = claimsPrincipal.FindFirstValue(ClaimTypes.Email);
         Role = claimsPrincipal.FindFirstValue(ClaimTypes.Role);
     }
 
-    public string? Role { get; set; }
+    public string Role { get; set; }
 
-    public string? Email { get; set; }
+    public string Email { get; set; }
 
-    public string? UserName { get; set; }
+    public string UserName { get; set; }
 
-    public Guid? UserId { get; set; }
+    public Guid UserId { get; set; }
+
+    public void ValidateIfEditor()
+    {
+        var allowedRoles = new List<string>() { UserRolesConstants.Admin, UserRolesConstants.Editor };
+        if (!allowedRoles.Contains(Role))
+        {
+            throw new UnauthorizedAccessException();
+        }
+    }
+
+    public void ValidateIfCustomer()
+    {
+        var allowedRoles = new List<string>() { UserRolesConstants.Customer };
+        if (!allowedRoles.Contains(Role))
+        {
+            throw new UnauthorizedAccessException();
+        }
+    }
 }
